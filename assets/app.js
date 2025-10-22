b893be69ecf1aded6e61799ee3b240d91202e901
neymarviado <geovani.garcia@escola.pr.gov.br>
2025-10-22 08:50:49 -0300

    gh

---
+++ assets/app.js
@@ -0,0 +1,4 @@
+function criacartao(categoria,pergunta,resposta) {
+    let container=document.getelementbyld('container')
+    let cartao=document.createelement('article')
+    cartao.classname='cartao'
