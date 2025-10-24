49137a8919d841577cafc98bf022e1535c7ae979
neymarviado <geovani.garcia@escola.pr.gov.br>
2025-10-24 10:31:36 -0300

    reyj

--- pergunta.js
+++ pergunta.js
@@ -1,9 +1,20 @@
 criacartao(
+    'progamação',
+    ' oque é python?',
+    'o pythoné uma limguagem de progamação
+)
+criacartao(
     'geografia',
-    qual a capital da frança?´ ,
+    qual a capital da frança ?´ ,
     'a capital da frança e paris'
 )
 criacartao(
     'progamaçao',
     'o que e uma funçao?',
-    '
+    ' uma funcão e um bloco de codigo que executa alguma tarefa'
+)
+criacartao(
+    lingua inglesa',
+     'como se diz oi em inglês?',
+    'oi em ingles é hi (rai)'
+)
