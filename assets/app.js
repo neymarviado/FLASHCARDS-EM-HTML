f33fd99340fc16b07ecbbc7b27b11a630cec3405
neymarviado <geovani.garcia@escola.pr.gov.br>
2025-10-24 10:39:25 -0300

    yyh

--- assets/app.js
+++ assets/app.js
@@ -2,3 +2,13 @@ function criacartao(categoria,pergunta,resposta) {
     let container=document.getelementbyld('container')
     let cartao=document.createelement('article')
     cartao.classname='cartao'
+    cartao.innerhtml='
+    <div class="cartao__conteudo">
+    <h3>${ categoria}</h3>
+    <div class=cartao__conteudo__pergunta">
+    <p>${pergunta}</p>
+    </div>
+    <div class="cartao__conteudo__resposta">
+        <p>${resposta}</p>
+        </div>
+        </div>
